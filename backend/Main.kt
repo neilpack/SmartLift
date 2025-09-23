9975ed5da33b8e806ad50f8d972d9a213079a985
neilpack <orangemustang11@gmail.com>
2025-09-23 13:57:03 -0600

    added more to Kotlin file

--- backend/Main.kt
+++ backend/Main.kt
@@ -2,4 +2,13 @@
 
 fun main() {
   println("Hello World")
+    math()
+}
+fun math() {
+    var name = "John"      // String (text)
+    val birthyear = 1975   // Int (number)
+
+    println(name)          // Print the value of name
+    println(birthyear)     // Print the value of birthyear
+    println("")
 }
