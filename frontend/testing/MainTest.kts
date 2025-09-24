e3c9ce3cb741254a3eb6a33f4e2b647dacf244a7
neilpack <orangemustang11@gmail.com>
2025-09-24 11:43:23 -0600

    fixed kotlin kt error

---
+++ frontend/testing/MainTest.kts
@@ -0,0 +1,12 @@
+// fun is how you define a function in Kotlin
+main()
+fun main() {
+    println("Hello World")
+    math()
+}
+fun math() {
+    val a = 1
+    val b = 2
+    val c = a + b
+    println(c)
+}
