b240028eaf0df0b90b4efbc38743e67915b41589
MarkIsQuiet <mrowley621@gmail.com>
2025-10-02 17:22:58 -0600

    Update

--- backend/index.py
+++ backend/index.py
@@ -1,12 +1,12 @@
 # Install libraries
 import os
 
-os.system("pip install pandas")
-os.system("pip install numpy")
-os.system("pip install matplotlib")
-os.system("pip install seaborn")
-os.system("pip install plotly")
-os.system("pip install SQLAlchemy")
+#os.system("pip install pandas")
+#os.system("pip install numpy")
+#os.system("pip install matplotlib")
+#os.system("pip install seaborn")
+#os.system("pip install plotly")
+#os.system("pip install SQLAlchemy")
 
 # Call Libraries
 import pandas as pd
@@ -21,4 +21,37 @@ import sqlite3
 
 
 # Define Functions
+def callTableNames():
+    database = sqlite3.connect('exercises.db')
+    cursor = database.cursor()
+
+    cursor.execute("SELECT name FROM sqlite_master WHERE type ='table';")
+    tables = cursor.fetchall()
+
+    for table in tables:
+        print(table)
+
+    database.close
+
 def callTable(table):
+    database = sqlite3.connect('exercises.db')
+
+    #
+    df = pd.read_sql_query(f"SELECT * FROM {table}", database)
+    print(df)
+
+    database.close
+
+def callRow(table, row_id):
+    return 0
+
+def createExercisePlanned(exercise, quantity, measurement):
+    return 0
+
+def deleteExercisePlanned(exercise_id):
+    return 0
+
+def editExercisePlanned(exercise, quantity, measurement):
+    return 0
+
+callTableNames()
