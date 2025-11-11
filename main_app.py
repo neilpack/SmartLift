b6e1d8515e9cb7eec70b05bc92cb570e8c3d3693
Neil Pack <orangemustang11@gmail.com>
2025-11-11 13:15:57 -0700

    cleaned up main and deleted backend folder

---
+++ main_app.py
@@ -0,0 +1,44 @@
+import tkinter as tk
+from tkinter import messagebox
+import sqlite3
+import ttkbootstrap as ttk
+from ttkbootstrap import Window
+import os
+from demo.equipment_menu import EquipmentMenu
+from demo.exercise_menu import ExerciseMenu
+from demo.exercise_details import ExerciseDetails
+
+
+class SmartLiftApp:
+    def __init__(self):
+        self.root = Window(themename="darkly")
+        self.root.title("SmartLift")
+        self.root.geometry("1080x1920")
+        self.current_frame = None
+        self.current_equipment = None
+        self.show_equipment_menu()
+
+    def clear_frame(self):
+        if self.current_frame:
+            self.current_frame.destroy()
+
+    def show_equipment_menu(self):
+        self.clear_frame()
+        self.current_frame = EquipmentMenu(self.root, self.show_exercise_menu)
+
+    def show_exercise_menu(self, selected_equipment):
+        self.current_equipment = selected_equipment
+        self.clear_frame()
+        self.current_frame = ExerciseMenu(self.root, selected_equipment, self.show_exercise_details, self.show_equipment_menu)
+
+    def show_exercise_details(self, exercise_name):
+        self.clear_frame()
+        self.current_frame = ExerciseDetails(self.root, exercise_name, 
+                                           lambda: self.show_exercise_menu(self.current_equipment))
+
+    def run(self):
+        self.root.mainloop()
+
+if __name__ == "__main__":
+    app = SmartLiftApp()
+    app.run()
